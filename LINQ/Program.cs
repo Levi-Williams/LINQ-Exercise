24465c85b7cc17dcd1fa89f786099d3e68f85068
Levi Williams <ljw758@hotmail.com>
2023-04-10 02:14:53 -0500

    Completed Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameList = new List<string>();
+
+            videoGameList.Add("Dark Souls");
+            videoGameList.Add("Elden Ring");
+            videoGameList.Add("Stardew");
+            videoGameList.Add("Minecraft");
+            videoGameList.Add("League of Legends");
+
+            var longestName = videoGameList.OrderBy(name => name.Length).Last();
+
+           Console.Write(longestName);
+
         }
     }
 }
